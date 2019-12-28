28bcca50cec5d3b6bd725afbad332fb30323577e
Evan <supdeerlord@gmail.com>
2019-12-27 19:47:57 -0500

    Initial WIP

---
+++ src/components/Navigation.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>evanm.me</li>
+        </ul>
+        <ul id="nav-list">
+          <li>About me</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Navigation;
